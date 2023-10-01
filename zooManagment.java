5ba3e9978a19fcf43ff60cfbf0a8a9cc91adaf01
ala sahbeni <94371580+alasahbeni1@users.noreply.github.com>
2023-10-01 01:28:11 +0100

    Add files via upload

---
+++ zooManagment.java
@@ -0,0 +1,41 @@
+public class zooManagment {
+
+
+
+    public static void main(String[] args) {
+
+        Animal lion = new Animal();
+        lion.name = "Simba";
+        lion.age = 8;
+        lion.family = "Cats";
+        lion.isMammal = true;
+
+        zoo myZoo = new zoo();
+        myZoo.name = "Wildlife Park";
+        myZoo.city = "Ariana";
+        myZoo.nbrCages = 25;
+        for (Animal animal : myZoo.animals = new Animal[25]) {
+            
+        }
+
+
+        Animal dog = new Animal("Canine", "Snoopy", 2, true);
+
+        System.out.println(myZoo);
+        System.out.println(myZoo.toString());
+
+        System.out.println(myZoo.addAnimal(lion));
+        System.out.println(myZoo.addAnimal(dog));
+
+        myZoo.displayAnimals();
+
+        System.out.println(myZoo.searchAnimal(dog));
+        Animal dog2 = new Animal("Canine", "Snoopy", 2, true);
+        System.out.println(myZoo.searchAnimal(dog2));
+
+        System.out.println(myZoo.removeAnimal(dog));
+        myZoo.displayAnimals();
+
+    }
+
+}
